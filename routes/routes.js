6c0d0ffba958265ce638b5ef8afc14d42eeb35b1
AndreySuew <andreysuew@gmail.com>
2021-04-24 16:48:19 +0300

    --- Auto-Git Commit ---

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 router.get('/', (request, response) => {
     response.send({
-        message: 'Node.js and Express REST API'
+        message: 'Node.js and Express REST API',
+        args: `.${req.query.page}`
     });
 });
 
