785724ea5494ce695d55e23d320d94e38037c78c
AndreySuew <andreysuew@gmail.com>
2021-04-24 16:08:26 +0300

    asf

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const http = require('http');
 const port = 3001 || process.env.PORT;
 const server = http.createServer();
+
+server.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+ 
+    console.log(`Server is listening on port ${port}`)
+})
