336e6d7a7bb4031c94e3c36860d2ce3d4f2b19d7
AndreySuew <andreysuew@gmail.com>
2021-04-24 16:20:17 +0300

    eblan

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ const port = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-app.get('/', (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.send({message: 'Node.js and Express REST API'});
-});
+routes(app);
 
-// Start the server
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
  
