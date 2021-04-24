b4825d6644625f87315958033a3d00f7131c7ad3
AndreySuew <andreysuew@gmail.com>
2021-04-24 16:10:30 +0300

    asf

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const port = 3001 || process.env.PORT;
 const server = http.createServer();
-
+server.on('request', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.end('Hello, server!')
+})
 server.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
  
