833cf56f6fbeeafadce423acd111919b0f9f9c98
AndreySuew <andreysuew@gmail.com>
2021-04-24 20:33:43 +0300

    a

--- routes/routes.js
+++ routes/routes.js
@@ -16,7 +16,7 @@ router.get('/discord', (request, response) => {
     let options = {
         host: 'discordapp.com',
         path: '/api/users/' + request.query.id,
-        headers: {'Authorization': 'Bot NTgwMDUzMDA5MDc4Mjg4Mzk0.XOLGdg.jcA_4VkUYhJxOLO5nxvPGd8oUhk'}
+        headers: {'Content-Type': 'application/json', 'Authorization': 'Bot NTgwMDUzMDA5MDc4Mjg4Mzk0.XOLGdg.jcA_4VkUYhJxOLO5nxvPGd8oUhk'}
     };
     let users_data = http.request(options).end();
     
