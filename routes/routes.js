d99d81f8d06f2c3a537e940488efd476c9a57c45
Yokkkoso <yokko.jke@gmail.com>
2021-04-25 18:30:06 +1000

    fix(*)

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js')
+const { Client } = require('discord.js');
 const express = require('express');
 
 const router = express.Router();
@@ -10,9 +10,9 @@ router.get('/', (request, response) => {
 });
 
 router.get('/discord', async (request, response) => {
-    if(!request.query.type) return response.send({ error: 'Вы не указали тип, используйте user или avatar' });
-    if(!request.query.id) return response.send({ error: 'Вы не указали ID пользователя' });
-    const client = new Client()
+    if (!request.query.type) return response.send({ error: 'Вы не указали тип, используйте user или avatar' });
+    if (!request.query.id) return response.send({ error: 'Вы не указали ID пользователя' });
+    const client = new Client();
     await client.login('NTgwMDUzMDA5MDc4Mjg4Mzk0.XOLGdg.jcA_4VkUYhJxOLO5nxvPGd8oUhk');
 
     const id = request.query.id;
@@ -25,22 +25,22 @@ router.get('/discord', async (request, response) => {
         return response.send({ error: 'Указан неверный ID' });
     }
 
-    if(apiType === 'user') response.send({
+    if (apiType === 'user') response.send({
         id: request.query.id,
         data: fetchUser,
         apiType,
     });
 
-    if(apiType === 'avatar') {
+    if (apiType === 'avatar') {
         response.send({
             id: request.query.id,
             data: fetchUser.displayAvatarURL({ dynamic: true, format: 'png' }),
             apiType
-        })
+        });
      }
 });
 
-router.use(function(req, res){
+router.use((req, res) => {
     res.status(404).json({ error: 'Страница не найдена' });
 });
 
