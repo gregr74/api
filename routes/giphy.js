5f7f6d3159925f279da16f4bc13da48c1a0de664
Yokkkoso <yokko.jke@gmail.com>
2021-04-25 20:07:04 +1000

    feat(Random): Added random images from giphy

---
+++ routes/giphy.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+const giphyKey = 'C7bImfPsbaRlYZKx0o7iJw6OHHzk4sz0';
+
+module.exports = function(app) {
+
+    app.get('/random', async (req, res) => {
+        if (!req.query.q) return res.send({error: 'Вы не указали запрос'});
+        const giphyURL = `https://api.giphy.com/v1/gifs/random?api_key=${giphyKey}&tag=${req.query.q}&rating=r`;
+
+        const request = await fetch(giphyURL).then(res => res.json());
+        if (request.meta.status === '404') return res.status(404).send({ error: 'Картинки не найдено' });
+        res.send({
+            query: req.query.q,
+            data: request.data.image_url
+        });
+    });
+
+}
