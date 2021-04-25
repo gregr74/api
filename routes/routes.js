9956fe17a1f9bc6bb406e3aafbe326a0c6c255e4
Yokkkoso <yokko.jke@gmail.com>
2021-04-25 10:09:30 +1000

    refactor: Some refactor to code

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
+const { Client } = require('discord.js')
 const express = require('express');
-const router = express.Router();
-const {Client} = require('discord.js')
 
-try  {
+const router = express.Router();
+
 router.get('/', (request, response) => {
     response.send({
         message: 'Node.js and Express REST API',
@@ -10,29 +10,35 @@ router.get('/', (request, response) => {
     });
 });
 
-
-
 router.get('/discord', async (request, response) => {
-    if(!request.query.type) {response.send({error: 'Вы не указали тип, используйте user или avatar'});}
-    if(!request.query.id) {response.send({error: 'Вы не указали ID пользователя'});}
+    if(!request.query.type) return response.send({ error: 'Вы не указали тип, используйте user или avatar' });
+    if(!request.query.id) return response.send({ error: 'Вы не указали ID пользователя' });
     const client = new Client()
-    client.login('NTgwMDUzMDA5MDc4Mjg4Mzk0.XOLGdg.jcA_4VkUYhJxOLO5nxvPGd8oUhk');
-    id = request.query.id;
-    const fetchUser = await client.users.fetch(id);
-    apitype = request.query.type;
+    await client.login('NTgwMDUzMDA5MDc4Mjg4Mzk0.XOLGdg.jcA_4VkUYhJxOLO5nxvPGd8oUhk');
 
-    if(apitype == 'user') {response.send({fetchUser})};
+    const id = request.query.id;
+    const apiType = request.query.type;
 
-     if(apitype == 'avatar') {
+    let fetchUser;
+    try {
+       fetchUser = await client.users.fetch(id);
+    } catch (err) {
+        return response.send({ error: 'Указан неверный ID' });
+    }
+
+    if(apiType === 'user') response.send({
+        id: request.query.id,
+        data: fetchUser,
+        apiType,
+    });
+
+    if(apiType === 'avatar') {
         response.send({
-         data: fetchUser.displayAvatarURL(),
-         apitype: apitype,
-          id: `${request.query.id}`
-     })};
-
+            id: request.query.id,
+            data: fetchUser.displayAvatarURL({ dynamic: true, format: 'png' }),
+            apiType
+        })
+     }
 });
-    
-} catch (error) {
-  console.log(error);  
-}
+
 module.exports = router;
