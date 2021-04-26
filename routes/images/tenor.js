68745b9f4d094c918dd43db63f88ad49c879aae4
Yokkkoso <yokko.jke@gmail.com>
2021-04-26 20:45:30 +1000

    Костыль №3

---
+++ routes/images/tenor.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+const tenorKey = 'SDN6P5XCUNE1';
+
+module.exports = function(app) {
+
+    app.get('/tenor', async (req, res) => {
+        if (!req.query.q) return res.send({error: 'Вы не указали запрос'});
+        const tenorURL = `https://api.tenor.com/v1/random?q=${req.query.q}&key=${tenorKey}&limit=1`;
+
+        const request = await fetch(tenorURL).then(res => res.json());
+        console.log(request);
+        if (request.status === '404') return res.status(404).send({ error: 'Картинки не найдено' });
+        res.send({
+            query: req.query.q,
+            data: request.results[0].media[0].gif.url
+        });
+    });
+
+}
