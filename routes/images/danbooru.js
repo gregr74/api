31793724dcbfef6cc701c9c5245191427eff9fe1
Yokkkoso <yokko.jke@gmail.com>
2021-04-26 21:22:08 +1000

    Костыль №6

--- routes/images/danbooru.js
+++ routes/images/danbooru.js
@@ -6,10 +6,10 @@ module.exports = function(app) {
         if (!req.query.q) return res.send({error: 'Вы не указали запрос'});
 
         const danbooruURL = `https://danbooru.donmai.us/posts.json?random&tags=${req.query.q}&limit=100`
-        const random = Math.floor(Math.random() * 99);
 
         const request = await fetch(danbooruURL).then(res => res.json());
         if (request.length <= 0) return res.status(404).send({ error: 'Картинки не найдено' });
+        const random = Math.floor(Math.random() * (request.length - 1));
         res.send({
             query: req.query.q,
             data: request[random].file_url
