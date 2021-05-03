940c3e46d4a0caecd767015249b0fc7c5d7202d0
Yokkkoso <yokko.jke@gmail.com>
2021-05-03 21:59:18 +1000

    update

--- routes/samp/samp.js
+++ routes/samp/samp.js
@@ -1,16 +1,16 @@
+const query = require('samp-query')
 
-var query = require('samp-query')
 module.exports = function(app) {
 
     app.get('/samp', async (request, response) => {
         let validtoken = 'M9GouX7JatMwqUytQSpxrwAtHzOA1zXx070l51uorFyZMmDLBvwt8HWUaKmL2vPTDEnGMeIPZMIenkCrYzMy3S2P2SV1UT';
         if (!request.query.ip) return response.send({ error: 'Вы не указали IP сервера' });
         if (!request.query.port) return response.send({ error: 'Вы не указали порт сервера' });
-        if (request.query.token!=validtoken) return response.send({ error: 'Вы указали не верный token' });
+        if (request.query.token !== validtoken) return response.send({ error: 'Вы указали не верный token' });
         
         const ip = request.query.ip;
         const port = request.query.port;
-        var options = {
+        const options = {
             host: ip,
             port: port
         }
@@ -20,8 +20,7 @@ module.exports = function(app) {
                 ip: request.query.ip,
                 error: error
             });
-            } else { 
-            console.log(response)
+            } else {
             response.send({
                 ip: request.query.ip,
                 data: data
