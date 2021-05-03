75d77055d45378fbb632061484a5de52c6150942
AndreySuew <andreysuew@gmail.com>
2021-05-03 14:27:55 +0300

    --- Auto-Git Commit ---

--- routes/samp/samp.js
+++ routes/samp/samp.js
@@ -2,10 +2,11 @@ const { Client } = require('discord.js');
 
 module.exports = function(app) {
 
-    app.get('/darling', async (request, response) => {
+    app.get('/samp', async (request, response) => {
+        let validtoken = 'M9GouX7JatMwqUytQSpxrwAtHzOA1zXx070l51uorFyZMmDLBvwt8HWUaKmL2vPTDEnGMeIPZMIenkCrYzMy3S2P2SV1UT';
         if (!request.query.ip) return response.send({ error: 'Вы не указали IP сервера' });
         if (!request.query.port) return response.send({ error: 'Вы не указали порт сервера' });
-        if (!request.query.token) return response.send({ error: 'Вы не указали порт сервера' });
+        if (request.query.token!=validtoken) return response.send({ error: 'Вы указали не верный token' });
         
         const ip = request.query.ip;
         const port = request.query.port;
