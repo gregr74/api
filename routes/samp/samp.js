77b5d052206da3a1f95117468d4e349943c1e85b
Andrey Chuhalenko <39628283+Rxflex@users.noreply.github.com>
2021-06-08 15:50:09 +0300

    Update samp.js

--- routes/samp/samp.js
+++ routes/samp/samp.js
@@ -3,10 +3,8 @@ const query = require('samp-query')
 module.exports = function(app) {
 
     app.get('/samp', async (request, response) => {
-        let validtoken = 'M9GouX7JatMwqUytQSpxrwAtHzOA1zXx070l51uorFyZMmDLBvwt8HWUaKmL2vPTDEnGMeIPZMIenkCrYzMy3S2P2SV1UT';
         if (!request.query.ip) return response.send({ error: 'Вы не указали IP сервера' });
         if (!request.query.port) return response.send({ error: 'Вы не указали порт сервера' });
-        if (request.query.token !== validtoken) return response.send({ error: 'Вы указали не верный token' });
         
         const ip = request.query.ip;
         const port = request.query.port;
