3b18e7ce399c4618258efa8e773465ab81c03e2f
AndreySuew <andreysuew@gmail.com>
2021-10-06 23:35:12 +0300

    Instagram api fix

--- routes/instagram/instagram.js
+++ routes/instagram/instagram.js
@@ -11,15 +11,12 @@ module.exports = function(app) {
             "Content-Type" : "application/json",
             "User-Agent"   : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 OPR/79.0.4143.73"
         };
-        console.log(await fetch(`https://www.instagram.com/${username}/?__a=1`, {
-            method  : 'GET',
-            headers : headers
-        }).then(res=>res.text()));
 
         response.send({
             id: request.query.username,
             data: await fetch(`https://www.instagram.com/${username}/?__a=1`, {
                 method  : 'GET',
+                credentials: "same-origin",
                 headers : headers
             }).then(res=>res.text())
         });
