0aa4ff9e46364010c23103347da44d368eec1096
AndreySuew <andreysuew@gmail.com>
2021-10-06 23:58:18 +0300

    Instagram api fix

--- routes/instagram/instagram.js
+++ routes/instagram/instagram.js
@@ -7,8 +7,8 @@ module.exports = function(app) {
 
         const username = request.query.username;
         let headers = {
-            "Accept"       : "application/json",
-            "Content-Type" : "application/json",
+            "Accept"       : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Content-Type" : "application/json; charset=utf-8",
             "User-Agent"   : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 OPR/79.0.4143.73"
         };
 
