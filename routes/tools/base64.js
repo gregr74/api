f33aefb60ce6a31ab1ad1bafcd3a7272ce7d2963
AndreySuew <andreysuew@gmail.com>
2021-10-07 02:53:49 +0300

    Instagram api fix

---
+++ routes/tools/base64.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+const imageToBase64 = require('image-to-base64');
+
+
+module.exports = function(app) {
+
+    app.get('/base64', async (request, response) => {
+        if (!request.query.type) return response.send({ error: 'Вы не указали тип (decode/encode)' });
+        if (!request.query.inputlink) return response.send({ error: 'Вы не указали ссылку' });
+
+        const type = request.query.type;
+        const link = request.query.inputlink;
+
+        if(type=='encode') {
+                imageToBase64(link)
+                    .then( (res) => {
+                        response.send({
+                            input: link,
+                            output: res
+                        })
+                        }
+                    )
+                    .catch(
+                        (error) => {
+                            response.send({
+                                input: link,
+                                output: error
+                            })
+                            console.log(error);
+                        }
+                    )
+        }
+
+
+    });
+}
