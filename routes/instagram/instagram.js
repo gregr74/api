576b5c6f353e6b5ceb3fb7fb4bdeb78dc480e796
AndreySuew <andreysuew@gmail.com>
2021-10-06 23:21:30 +0300

    Instagram api fix

--- routes/instagram/instagram.js
+++ routes/instagram/instagram.js
@@ -6,10 +6,10 @@ module.exports = function(app) {
         if (!request.query.username) return response.send({ error: 'Вы не указали имя пользователя' });
 
         const username = request.query.username;
-console.log(await fetch(`https://www.instagram.com/${username}/?__a=1`));
+console.log(await fetch(`https://www.instagram.com/${username}/?__a=1`).then(res=>res.json()));
         response.send({
             id: request.query.username,
-            data: await fetch(`https://www.instagram.com/${username}/?__a=1`)
+            data: await fetch(`https://www.instagram.com/${username}/?__a=1`).then(res=>res.json())
         });
     });
 }
