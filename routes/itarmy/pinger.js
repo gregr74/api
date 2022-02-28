692a6ddb954da4af6352d73fddce939236b241c6
AndreySuew <andreysuew@gmail.com>
2022-02-28 19:08:34 +0200

    ITArmy pinger feature

---
+++ routes/itarmy/pinger.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+module.exports = function(app, memory) {
+    app.get('/pinger', async (request, response) => {
+        const array = {}; array.online = []; array.offline = []; let i = 0;
+        memory['requests']++
+        if (!request.query.data) return response.send({ error: 'Вы не указали входные данные' });
+
+        const data = request.query.data;
+
+        let json = JSON.parse(data);
+        json.forEach(website=>{
+            const request = require('request');
+            request({url:  website, timeout: 500}, function (error, resp, body) {
+                if (!error && resp.statusCode == 200) {
+                    i++;
+                    array.online.push(website);
+                    console.log(`${website} online`);
+                } else {
+                    i++;
+                    array.offline.push(website);
+                    console.log(`${website} offline`);
+                }
+                if(i==json.length) {
+                    response.send(array);
+                }
+            })
+        })
+
+
+    });
+}
