34ddf5c336b2c48dcde0b3da90d87845514689b0
AndreySuew <andreysuew@gmail.com>
2022-02-28 19:24:03 +0200

    fixed ITArmy pinger

--- routes/itarmy/pinger.js
+++ routes/itarmy/pinger.js
@@ -16,7 +16,7 @@ module.exports = function(app, memory) {
                 headers: {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
                 },
-                timeout: 1000
+                timeout: 1500
             }, function (error, resp, body) {
                 if (!error) {
                     i++;
