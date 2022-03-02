413a7f7ad7748cdc0ac0937b431889eb1006e810
AndreySuew <andreysuew@gmail.com>
2022-03-02 15:41:15 +0200

    ITArmy Pinger add proxy support Code refactored

--- routes/itarmy/pinger.js
+++ routes/itarmy/pinger.js
@@ -3,34 +3,32 @@ function isJSON(str) { try { return (JSON.parse(str) && !!str); } catch (e) { re
 
 module.exports = function(app, memory) {
     app.get('/pinger', async (request, response) => {
-        const array = {}; array.online = []; array.offline = []; let i = 0;
-        memory['requests']++
+        const array = {}; array.online = []; array.offline = []; let i = 0; memory['requests']++
         if (!request.query.data) return response.send({ error: 'Вы не указали входные данные' });
 
         const data = request.query.data;
+        let proxy = request.query.proxy || null;
         if(!isJSON(data)) {return response.send({"error": "Данные которые вы предоставили не являются JSON"})}
         let json = JSON.parse(data);
         json.forEach(website=>{
             const request = require('request');
-            request({
+            let proxy_request = request;
+            if(proxy !== null) proxy_request = request.defaults({'proxy': proxy});
+            proxy_request({
                 url:  website,
                 headers: {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
                 },
-                timeout: 1500
+                timeout: 200
             }, function (error, resp, body) {
-                if (!error) {
-                    i++;
+                if (!error) {i++;
                     array.online.push(website);
                     console.log(`${website} online [${resp.statusCode}]`);
-                } else {
-                    i++;
+                } else {i++;
                     array.offline.push(website);
                     console.log(`${website} offline [${error.code}]`);
                 }
-                if(i==json.length) {
-                    response.send(array);
-                }
+                if(i==json.length) response.send(array);
             })
         })
 
